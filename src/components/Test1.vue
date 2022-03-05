7a5f31d45c543e154273626e9513b8ac4be109af
uxin1001 <1329220172@qq.com>
2022-03-05 22:03:13 +0800

    。。。

---
+++ src/components/Test1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+{{message}}
+  </div>
+
+</template>
+
+<script>
+export default {
+  name:'Test1',
+  data() {
+    return{
+      message:"helloworld"
+    }
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>

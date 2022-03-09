00d1d02b030df03f7dc873366199d3aaf4822511
uxin <uxin@163.com>
2022-03-10 00:36:22 +0800

    测试2

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test2',
+      name: 'Test2',
+      component:()=>import("../components/Test2.vue"),
     }
   ]
 })
